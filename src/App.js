c6467fef115c9b99ec5c001ed79ab4a1e7c03cd1
Rachel J. Scherer <rjscherer@fdn.cpp.edu>
2019-10-17 16:42:28 -0700

    Gutted default react app, created weathermodule.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 import './App.css';
+import { weathermodule } from './weathermodule'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        <br />
+        <br />
+        <h2>Welcome to weatherpi :)</h2>
+        <Router>
+          <weathermodule />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/weathermodule.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class weathermodule extends Component {
+    render() {
+        return (
+            <div>
+                <p>This is the weather module</p>
+            </div>
+        );
+    }
+}
+
+export default weathermodule;
