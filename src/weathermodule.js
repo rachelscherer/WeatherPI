117db4cfd5cf55a35fbafb3ebbcdf7cb452bc8b1
Rachel Scherer <rachelscherer94@gmail.com>
2019-10-18 13:39:45 -0700

    Started creating map

--- src/weathermodule.js
+++ src/weathermodule.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
+import { Map, mapStyles, GoogleApiWrapper } from 'google-maps-react';
 
 class Weathermodule extends Component {
     render() {
         return (
             <div>
                 <p>This is the weather module</p>
+                <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
             </div>
         );
     }
 }
-
+{/*
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyA6eUcJv6lgP5Q96Wu6fSOhv36BbfzGOSU'
+  })(MapContainer);
+  */}
 export default Weathermodule;
