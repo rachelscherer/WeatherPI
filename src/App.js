692eefb71757ef8b613b2191043d1dd4a122d898
Rachel Scherer <rachelscherer94@gmail.com>
2019-10-17 17:00:54 -0700

    Added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { weathermodule } from './weathermodule'
+import Weathermodule from './weathermodule'
 
 class App extends Component {
   render(){
     return (
       <div className="App">
-        <br />
-        <br />
-        <h2>Welcome to weatherpi :)</h2>
-        <Router>
-          <weathermodule />
-        </Router>
+        <Container>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <h2>Welcome to WeatherPi :)</h2>
+          <hr />
+          <Weathermodule />
+        </Container>
       </div>
     );
   }
--- src/weathermodule.js
+++ src/weathermodule.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class weathermodule extends Component {
+class Weathermodule extends Component {
     render() {
         return (
             <div>
@@ -10,4 +10,4 @@ class weathermodule extends Component {
     }
 }
 
-export default weathermodule;
+export default Weathermodule;
