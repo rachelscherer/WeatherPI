1d2bd910ef914b28011b078d4dd13df5da910690
Rachel Scherer <rscherer1@student.rccd.edu>
2019-10-18 18:03:24 -0700

    Formatted it, added stats. Made it look pretty.

--- src/App.js
+++ src/App.js
@@ -9,15 +9,13 @@ class App extends Component {
     return (
       <div className="App">
         <Container>
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
-          <br />
           <h2 className="header">Welcome to WeatherPi</h2>
           <hr />
+          <br />
           <Weathermodule />
+          <p>
+            Learn more about the developers <a href="#">here.</a>
+          </p>
         </Container>
       </div>
     );
--- src/weathermodule.js
+++ src/weathermodule.js
@@ -1,38 +1,46 @@
 import React, { Component } from "react";
 import { Map, GoogleApiWrapper } from "google-maps-react";
+import { Container, Row, Col } from "react-bootstrap";
 import "./weathermodule.css";
 
 //Set the width and height of the map
 const mapStyles = {
-  width: "40vh",
-  height: "40vh"
+  width: "395px",
+  height: "395px",
+  margin: "0 0 0 50px"
 };
 
 class Weathermodule extends Component {
   render() {
     return (
       <div className="weatherModule">
-        <Map
-          google={this.props.google}
-          zoom={17}
-          style={mapStyles}
-          initialCenter={{ lat: 34.058984, lng: -117.81954 }}
-        />
-        <p>Test</p>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        <Container>
+          <Row>
+            <Col md={12} lg={6}>
+              <div className="location">
+                <h2>Location</h2>
+                <br />
+                <Map
+                  google={this.props.google}
+                  zoom={17}
+                  style={mapStyles}
+                  initialCenter={{ lat: 34.058984, lng: -117.81954 }}
+                  disableDefaultUI={true}
+                />
+              </div>
+            </Col>
+            <Col md={12} lg={6}>
+              <h2>Stats</h2>
+              <br />
+              <p className="stats">Temperature: xxxxxx</p>
+              <p className="stats">Humidity: xxxxxx</p>
+              <p className="stats">Wind Speed: xxxxxx</p>
+              <p className="stats">Stat 4: xxxxxx</p>
+              <p className="stats">Stat 5: xxxxxx</p>
+              <p className="stats">Stat 6: xxxxxx</p>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
